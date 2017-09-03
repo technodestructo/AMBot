06457b8b96e0b28b3e8d2e35e1fd0cf5cb58cc9f
technodestructo <31578931+technodestructo@users.noreply.github.com>
2017-09-03 10:54:08 -0600

    EIGHTS VERSION
    as requested

--- am_bot.js
+++ am_bot.js
@@ -39,7 +39,7 @@ function initGUI() {
 	clearInterval(GUIsetup);
 	
 	$("#AMBotContainer").append('<div id="AMBotTitleTab" sytle="position:absolute; right:0; top:0; bottom:0; width:30px">');
-	$("#AMBotTitleTab").append('<p style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg); margin-top:-104px; margin-right:-63px; font-family:Helvetica; font-size:20pt;">AMBotv1</p>');
+	$("#AMBotTitleTab").append('<p style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg); margin-top:-104px; margin-right:-63px; font-family:Helvetica; font-size:20pt;">AMBot eights</p>');
 }
 
 function startAMBot() {
