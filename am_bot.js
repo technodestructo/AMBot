289be264373d965e37ab519883af6a3effcea117
technodestructo <31578931+technodestructo@users.noreply.github.com>
2017-09-03 20:38:48 -0600

    fixing so not tarded
    trus me guise

--- am_bot.js
+++ am_bot.js
@@ -75,13 +75,11 @@ function clickSecondSoftReset() {
 }
 
 function clickSoftReset() {
-	if ($("#resetLabel").html() === "Dimension Boost: requires 71 Eighth Dimensions" && $("#secondResetLabel").html() === "Antimatter Galaxies: requires 71 Eighth Dimensions") {
-		clearInterval(doSoftReset);
-	} else if ($("#resetLabel").html() === "Dimension Boost: requires 86 Eighth Dimensions" && $("#secondResetLabel").html() === "Antimatter Galaxies: requires 131 Eighth Dimensions"){
-		clearInterval(doSoftReset);
-	} else {
+	if ($("#resetLabel").html() != "Dimension Boost: requires 71 Eighth Dimensions" && $("#secondResetLabel").html() != "Antimatter Galaxies: requires 71 Eighth Dimensions") {
 		$("#softReset").click();
-	}
+	} else if ($("#resetLabel").html() != "Dimension Boost: requires 86 Eighth Dimensions" && $("#secondResetLabel").html() != "Antimatter Galaxies: requires 131 Eighth Dimensions") {
+		$("#softReset").click();
+	} 
 }
 
 /*
