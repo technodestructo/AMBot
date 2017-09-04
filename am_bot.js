1bdfef1caa42cca4b090fd9829c92dff99f4b722
technodestructo <31578931+technodestructo@users.noreply.github.com>
2017-09-03 21:08:53 -0600

    pretty

--- am_bot.js
+++ am_bot.js
@@ -39,7 +39,7 @@ function initGUI() {
 	clearInterval(GUIsetup);
 	
 	$("#AMBotContainer").append('<div id="AMBotTitleTab" sytle="position:absolute; right:0; top:0; bottom:0; width:30px">');
-	$("#AMBotTitleTab").append('<p style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg); margin-top:-104px; margin-right:-73px; font-family:Helvetica; font-weight:Light; font-size:20pt;">AMBot supreme</p>');
+	$("#AMBotTitleTab").append('<p style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg); margin-top:-204px; margin-right:-73px; font-family:Helvetica; font-weight:Light; font-size:20pt;">AMBot supreme</p>');
 }
 
 function startAMBot() {
